5896c72abb1e9974337f0fd94952164542a404fd
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2021-02-05 16:15:38 -0600

    Update
    >Vector3 & Vector3i

---
+++ giUtilities/include/giVector3i.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "giPlatformMath.h"
+
+using giEngineSDK::int32;
+
+class giVector3i
+{
+public:
+
+  giVector3i() = default;
+
+  giVector3i(int32 inX, int32 inY, int32 inZ);
+
+  giVector3i(giVector3i& inVect);
+
+  ~giVector3i();
+
+  int32
+  getX() {
+    return x;
+  }
+
+  int32
+  getY() {
+    return y;
+  }
+
+  int32
+  getZ() {
+    return z;
+  }
+
+  void
+  setX(int32 inVal) {
+    x = inVal;
+  }
+
+  void
+  setY(int32 inVal) {
+    y = inVal;
+  }
+
+  void
+  setZ(int32 inVal) {
+    z = inVal;
+  }
+
+
+  giVector3i
+  operator+(const giVector3i& otherVect);
+  
+  giVector3i
+  operator-(const giVector3i& otherVect);
+  
+  giVector3i
+  operator*(const giVector3i& otherVect);
+  
+  giVector3i
+  operator/(const giVector3i& otherVect);
+  
+  
+  giVector3i
+  operator+(const int32& otherVal);
+  
+  giVector3i
+  operator-(const int32& otherVal);
+
+  giVector3i
+  operator*(const int32& otherVal);
+  
+  giVector3i
+  operator/(const int32& otherVal);
+  
+  
+  
+  bool
+  operator>=(const giVector3i& otherVect);
+  
+  bool
+  operator<=(const giVector3i& otherVect);
+  
+  bool
+  operator==(const giVector3i& otherVect);
+  
+  bool
+  operator<(const giVector3i& otherVect);
+  
+  bool
+  operator>(const giVector3i& otherVect);
+  
+  bool
+  operator>=(const int32& otherVal);
+  
+  bool
+  operator<=(const int32& otherVal);
+  
+  bool
+  operator==(const int32& otherVal);
+  
+  bool
+  operator<(const int32& otherVal);
+  
+  bool
+  operator>(const int32& otherVal);
+ 
+  int32
+  dotProd(const giVector3i& inVect);
+  
+  int32
+  crossProd(const giVector3i& inVect);
+  
+  void
+  normalize();
+  
+  int32
+  magnitude();
+
+
+private:
+  int32 x;
+  int32 y;
+  int32 z;
+
+};
---
+++ giUtilities/src/giVector3.cpp
@@ -0,0 +1,195 @@
+#include "giVector3.h"
+
+
+giVector3::giVector3(float inX, float inY, float inZ) {
+  x = inX;
+  y = inY;
+  z = inZ;
+}
+
+giVector3::giVector3(giVector3& inVect) {
+  x = inVect.getX();
+  y = inVect.getY();
+  z = inVect.getZ();
+}
+
+giVector3::~giVector3() {
+
+}
+
+giVector3
+giVector3::operator+(const giVector3& otherVect)
+{
+  return giVector3(x + otherVect.x, y + otherVect.y, z + otherVect.z);
+}
+
+giVector3
+giVector3::operator-(const giVector3& otherVect)
+{
+  return giVector3(x - otherVect.x, y - otherVect.y, z - otherVect.z);
+}
+
+giVector3
+giVector3::operator*(const giVector3& otherVect)
+{
+  return giVector3(x * otherVect.x, y * otherVect.y, z * otherVect.z);
+}
+
+giVector3
+giVector3::operator/(const giVector3& otherVect)
+{
+  return giVector3(x / otherVect.x, y / otherVect.y, z / otherVect.z);
+}
+
+giVector3
+giVector3::operator+(const float& otherVal)
+{
+  return giVector3(x + otherVal, y + otherVal, z + otherVal);
+}
+
+giVector3
+giVector3::operator-(const float& otherVal)
+{
+  return giVector3(x - otherVal, y - otherVal, z - otherVal);
+}
+
+giVector3
+giVector3::operator*(const float& otherVal)
+{
+  return giVector3(x * otherVal, y * otherVal, z * otherVal);
+}
+
+giVector3
+giVector3::operator/(const float& otherVal)
+{
+  return giVector3(x / otherVal, y / otherVal, z / otherVal);
+}
+
+bool
+giVector3::operator>=(const giVector3& otherVect)
+{
+  if (otherVect.x <= x && otherVect.y <= y && otherVect.z <= z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator<=(const giVector3& otherVect)
+{
+  if (otherVect.x >= x && otherVect.y >= y && otherVect.z >= z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator==(const giVector3& otherVect)
+{
+  if (otherVect.x == x && otherVect.y == y && otherVect.z == z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator<(const giVector3& otherVect)
+{
+  if (otherVect.x > x && otherVect.y > y && otherVect.z > z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator>(const giVector3& otherVect)
+{
+  if (otherVect.x < x && otherVect.y < y && otherVect.z < z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator>=(const float& otherVal)
+{
+  if (otherVal <= x && otherVal <= y && otherVal <= z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator<=(const float& otherVal)
+{
+  if (otherVal >= x && otherVal >= y && otherVal >= z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator==(const float& otherVal)
+{
+  if (otherVal == x && otherVal == y && otherVal == z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator<(const float& otherVal)
+{
+  if (otherVal > x && otherVal > y && otherVal > z) {
+    return true;
+  }
+  return false;
+}
+
+bool
+giVector3::operator>(const float& otherVal)
+{
+  if (otherVal < x && otherVal < y && otherVal < z) {
+    return true;
+  }
+  return false;
+}
+
+float
+giVector3::dotProd(const giVector3& inVect)
+{
+  return ((x * inVect.x) + (y * inVect.y)+ (z * inVect.z));
+}
+
+float
+giVector3::crossProd(const giVector3& inVect)
+{
+  return (((y * inVect.z) - (z * inVect.y)),
+          ((z * inVect.x) - (x * inVect.z)),
+          ((x * inVect.y) - (y * inVect.z)));
+}
+
+void
+giVector3::normalize()
+{
+  float mag = magnitude();
+  if (mag > 0) {
+    float tmp = 1.0f / mag;
+    x *= tmp;
+    y *= tmp;
+    z *= tmp;
+  }
+  else {
+    x = 0.0f;
+    y = 0.0f;
+    z = 0.0f;
+  }
+}
+
+float
+giVector3::magnitude()
+{
+  return sqrtf(powf(x, 2) + powf(y, 2) + powf(z, 2));
+}
+
+
