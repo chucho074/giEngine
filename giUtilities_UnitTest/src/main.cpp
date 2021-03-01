ef877d59e7a0cdb34011d2b1cb025418085fd774
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2021-03-01 14:48:27 -0600

    Update

--- giUtilities_UnitTest/src/main.cpp
+++ giUtilities_UnitTest/src/main.cpp
@@ -1,3 +1,5 @@
+#define GTEST_HAS_TR1_TUPLE 0
+#define GTEST_USE_OWN_TR1_TUPLE 0
 #include <gtest/gtest.h>
 
 #include <giPrerequisitesUtilities.h>
@@ -20,6 +22,7 @@ main(int argc, char** argv) {
   return RUN_ALL_TESTS();
 }
 
+
 TEST(giUtilities, BasicTest) {
   EXPECT_EQ(sizeof(unsigned char), 1U);
   EXPECT_EQ(sizeof(uint8), 1U);
@@ -39,5 +42,6 @@ TEST(giUtilities, BasicTest) {
 
   //EXPECT_EQ(static_cast<uint32>(-1), NumLimit::MAX_UINT32);
   //EXPECT_FLOAT_EQ(Math::PI, 3.141592f);
-
+  
+  
 }
